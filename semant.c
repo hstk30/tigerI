ba5ac8b36db3b3fc4a86957bb0e98d8bd72d159d
hanwei <hstk30@gmail.com>
2022-09-23 22:54:10 +0800

    feat: 实现tydec

--- semant.c
+++ semant.c
@@ -24,11 +24,17 @@ void transDec(S_table venv, S_table tenv, A_dec d);
 Ty_ty transTy(S_table tenv, A_ty a);
 
 /*
+ * test16.tig
  * skip all `Ty_name` type
+ * TODO: recognize recursion type dec?
  */ 
 static Ty_ty
 actual_ty(Ty_ty ty) {
-
+    Ty_ty actual = ty;
+    while (actual->kind != Ty_name) {
+        actual = actual->u.name.ty;
+    }
+    return actual;
 }
 
 
@@ -105,14 +111,14 @@ transIfExp(S_table venv, S_table tenv, A_exp if_exp) {
     if (if_exp->u.iff.elsee) {
         struct expty else_expty = transExp(venv, tenv, if_exp->u.iff.elsee);
         if (then_expty.ty->kind != else_expty.ty->kind) {
-            EM_error(if_exp->pos, "IF THEN ELSE clause then else type dismatch");
+            EM_error(if_exp->pos, "IF THEN ELSE clause THEN ELSE type dismatch");
             return expTy(NULL, Ty_Void());
         } else {
             return expTy(NULL, then_expty.ty);
         }
     } else {
         if (then_expty.ty->kind != Ty_void) {
-            EM_error(if_exp->pos, "IF THEN clause then must return void");
+            EM_error(if_exp->pos, "IF THEN clause THEN must return void");
         }
         return expTy(NULL, Ty_Void());
     }
@@ -175,8 +181,54 @@ transLetExp(S_table venv, S_table tenv, A_exp let_exp) {
     return body_expty;
 }
 
+
+static struct expty
+transSimpleVar(S_table venv, S_table tenv, A_var simple_var) {
+    E_enventry x = S_look(venv, simple_var->u.simple);
+
+    if (x && x->kind == E_varEntry) {
+        return expTy(NULL, actual_ty(x->u.var.ty));
+    } else {
+        EM_error(simple_var->pos, "Variable %s use before declare", S_name(simple_var->u.simple));
+        /* default int ? */
+        return expTy(NULL, Ty_Int());
+    }
+}
+
+/*
+ * var type has the `sym` field?
+ */
+static struct expty
+transFieldVar(S_table venv, S_table tenv, A_var field_var) {
+    struct expty record_expty = transVar(venv, tenv, field_var->u.field.var);
+
+}
+
+/*
+ * index `exp` must be `int`
+ */
+static struct expty
+transSubscriptVar(S_table venv, S_table tenv, A_var subscript_var) {
+    struct expty array_expty = transVar(venv, tenv, subscript_var->u.subscript.var);
+    if (array_expty.ty->kind != Ty_array) {
+        EM_error(subscript_var->pos, "Array subscript array type required");
+        return expTy(NULL, Ty_Int());
+    }
+
+    struct expty idx_expty = transExp(venv, tenv, subscript_var->u.subscript.exp);
+    if (idx_expty.ty->kind != Ty_int) {
+        EM_error(subscript_var->pos, "Array index int type required");
+        return expTy(NULL, Ty_Int());
+    }
+    return expTy(NULL, array_expty.ty->u.array);
+}
+
 struct expty transVar(S_table venv, S_table tenv, A_var v) {
     switch (v->kind) {
+        case A_simpleVar: return transSimpleVar(venv, tenv, v);
+        case A_fieldVar: return transFieldVar(venv, tenv, v);
+        case A_subscriptVar: return transSubscriptVar(venv, tenv, v);
+        default: assert(0);
     }
 }
 
@@ -192,10 +244,11 @@ struct expty transExp(S_table venv, S_table tenv, A_exp a) {
         case A_arrayExp: return transArrayExp(venv, tenv, a);
         case A_seqExp: {
             A_expList el;
+            struct expty last_expty;
             for (el = a->u.seq; el; el = el->tail) {
-                transExp(venv, tenv, el->head);
+                last_expty = transExp(venv, tenv, el->head);
             }
-            return expTy(NULL, Ty_Void());
+            return expTy(NULL, last_expty.ty);
         }
         case A_assignExp: {
             struct expty var_expty = transVar(venv, tenv, a->u.assign.var); 
@@ -222,6 +275,7 @@ transVarDec(S_table venv, S_table tenv, A_dec var_dec) {
 
 /*
  * iter `params` 
+ * `A_fieldList` -> `Ty_tyList`
  * `A_field.typ` -> `Ty_ty`
  */
 static Ty_tyList
@@ -285,12 +339,27 @@ transFuncDec(S_table venv, S_table tenv, A_dec func_dec) {
  */
 static void
 transTyDec(S_table venv, S_table tenv, A_dec ty_dec) {
-    A_nametyList tl;
-    Ty_ty temp_ty;
+    A_nametyList iter;
+    Ty_ty t1, t2;
 
-    for (tl = ty_dec->u.type; tl; tl = ty_dec->u.type->tail) {
-        temp_ty = transTy(tenv, tl->head->ty);
-        S_enter(tenv, tl->head->name, temp_ty);
+    /* first time just add name to `tenv` */
+    for (iter = ty_dec->u.type; iter; iter = ty_dec->u.type->tail) {
+        t1 = S_look(tenv, iter->head->name);
+        if (t1 && t1->kind != Ty_name) {
+            /* In fact, temp_ty just will be int string or nil */
+            S_enter(tenv, iter->head->name, Ty_Name(iter->head->name, t1));
+        } else {
+            S_enter(tenv, iter->head->name, Ty_Name(iter->head->name, NULL));
+        }
+    }
+
+    /* second time set right type to `Ty_name` type */
+    for (iter = ty_dec->u.type; iter; iter = ty_dec->u.type->tail) {
+        t1 = S_look(tenv, iter->head->name);
+        if (t1 && t1->kind == Ty_name && t1->u.name.ty == NULL) {
+            t2 = transTy(tenv, iter->head->ty);
+            t1->u.name.ty = t2;
+        }
     }
 }
 
@@ -303,20 +372,66 @@ void transDec(S_table venv, S_table tenv, A_dec d) {
     }
 }
 
+static Ty_ty
+transNameTy(S_table tenv, A_ty name_ty) {
+    Ty_ty t = S_look(tenv, name_ty->u.name);
+    if (t) {
+        if (t->kind != Ty_name || t->u.name.ty) {
+            return Ty_Name(name_ty->u.name, t);
+        } else {
+            /* test16.tig */
+            EM_error(name_ty->pos, "Type %s circle declare", S_name(name_ty->u.name));
+            return Ty_Name(name_ty->u.name, Ty_Int());
+        }
+    } else {
+        EM_error(name_ty->pos, "Type %s use before declare", S_name(name_ty->u.name));
+        return Ty_Name(name_ty->u.name, Ty_Int());
+    }
+}
+
+/* 
+ * `A_fieldList` -> `Ty_fieldList`
+ */
+static Ty_fieldList 
+makeTyFieldList(S_table tenv, A_fieldList field_list) {
+    if (field_list == NULL)
+        return NULL;
+    Ty_ty field_ty = S_look(tenv, field_list->head->typ);
+    if (field_ty == NULL) {
+        EM_error(field_list->head->pos, "Type %s use before declare", S_name(field_list->head->typ));
+        field_ty = Ty_Int();
+    } 
+    return Ty_FieldList(
+        Ty_Field(field_list->head->name, field_ty), 
+        makeTyFieldList(tenv, field_list->tail)
+    );
+}
+
+/*
+ * NOTE: type-id {} -> Ty_Record(NULL)
+ */
+static Ty_ty
+transRecordTy(S_table tenv, A_ty record_ty) {
+    Ty_fieldList t = makeTyFieldList(tenv, record_ty->u.record);
+    return Ty_Record(t);
+}
+
+static Ty_ty
+transArrayTy(S_table tenv, A_ty array_ty) {
+    Ty_ty t = S_look(tenv, array_ty->u.array);
+    if (t == NULL) {
+        EM_error(array_ty->pos, "Type %s use before declare", S_name(array_ty->u.array));
+        /* TODO: return what when it happen? */
+        return Ty_Array(Ty_Int());
+    }
+    return Ty_Array(t);
+}
+
 Ty_ty transTy(S_table tenv, A_ty t) {
     switch (t->kind) {
-        case A_nameTy: {
-            Ty_ty name_ty = S_look(tenv, t->u.name);
-            if (name_ty == NULL) {
-                return Ty_Name(t->u.name, NULL);
-            } else {
-                return name_ty;
-            }
-        }
-        case A_recordTy: {
-
-        }
-        case A_arrayTy:
+        case A_nameTy: return transNameTy(tenv, t);
+        case A_recordTy: return transRecordTy(tenv, t);
+        case A_arrayTy: return transArrayTy(tenv, t);
         default: assert(0);
     }
 }
